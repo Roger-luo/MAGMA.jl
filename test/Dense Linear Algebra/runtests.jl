ce8dadcb732f019255b91f09ed24ac51aa5c8ee9
Jerry <582020970@qq.com>
2019-05-31 23:34:13 +0800

    Constructed the tests hierarchy and encoded the original test.jl file.

---
+++ test/Dense Linear Algebra/runtests.jl
@@ -0,0 +1,6 @@
+using MAGMA
+using Test, LinearAlgebra, Random
+
+@testset "Singular Value Decomposition" begin
+    include("Singluar Value Decomposition/runtests.jl")
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using MAGMA
-using Test
+using Test, Random, LinearAlgebra
 
+# for simply tests the very beginning codes for gesvd
 @testset "MAGMA.jl" begin
     # Write your own tests here.
+    include("Dense Linear Algebra/runtests.jl")
 end
