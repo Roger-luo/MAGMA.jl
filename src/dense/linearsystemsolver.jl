875c441cef3ea08dfb06e040835cce3b8bc41dd2
Jerry <chenys13@outlook.com>
2019-08-26 20:43:30 -0400

    Deleted some unnecessary sentences in linearsystemsolver.jl

--- src/dense/linearsystemsolver.jl
+++ src/dense/linearsystemsolver.jl
@@ -33,14 +33,6 @@ for type in magmaTypeList
                     A, max(1,stride(A,2)), B, max(1,stride(B,2)),
                     work, lwork, info
                 )
-                # ccall((@magmafunc_gpu($gels), libmagma), Cint,
-                #       (Cint, Cint, Cint, Cint,
-                #        PtrOrCuPtr{$elty}, Cint, PtrOrCuPtr{$elty}, Cint,
-                #        Ptr{$elty}, Cint, Ptr{Cint}),
-                #       MagmaNoTrans, m, n, size(B,2),
-                #       A, max(1,stride(A,2)), B, max(1,stride(B,2)),
-                #       work, lwork, info)
-
                 if i == 1
                     lwork = ceil(Int, real(work[1]))
                     resize!(work, lwork)
@@ -72,14 +64,6 @@ for type in magmaTypeList
                     A, max(1,stride(A,2)), B, max(1,stride(B,2)),
                     work, lwork, info
                 )
-                # ccall((@magmafunc($gels), libmagma), Cint,
-                #       (Cint, Cint, Cint, Cint,
-                #        PtrOrCuPtr{$elty}, Cint, PtrOrCuPtr{$elty}, Cint,
-                #        Ptr{$elty}, Cint, Ptr{Cint}),
-                #       MagmaNoTrans, m, n, size(B,2),
-                #       A, max(1,stride(A,2)), B, max(1,stride(B,2)),
-                #       work, lwork, info)
-
                 if i == 1
                     lwork = ceil(Int, real(work[1]))
                     resize!(work, lwork)
@@ -112,13 +96,13 @@ for type in magmaTypeList
         #               = 0: successful exit
         #               < 0: if INFO = -i, the i-th argument had an illegal value
         function magma_gesv!(A::CuArray{$elty}, B::CuArray{$elty})
-            # require_one_based_indexing(A, B)
-            # chkstride1(A, B)
+            @assert !has_offset_axes(A, B)
+            chkstride1(A, B)
             n = checksquare(A)
             isGPU = (A isa CuArray && B isa CuArray)
-            # if size(B,1) != n
-            #     throw(DimensionMismatch("B has leading dimension $(size(B,1)), but needs $n"))
-            # end
+            if size(B,1) != n
+                throw(DimensionMismatch("B has leading dimension $(size(B,1)), but needs $n"))
+            end
             lda  = max(1,stride(A,2))
             ldb  = max(1,stride(B,2))
             ipiv = similar(Matrix(A), Int32, n)
@@ -130,25 +114,17 @@ for type in magmaTypeList
                 A, lda, ipiv,
                 B, ldb, info
             )
-            # ccall((@magmafunc_gpu($gesv), libmagma), Cint,
-            #       (Cint, Cint,
-            #        PtrOrCuPtr{$elty}, Cint, Ptr{Cint},
-            #        PtrOrCuPtr{$elty}, Cint, Ptr{Cint}),
-            #        n, size(B,2),
-            #        A, lda, ipiv,
-            #        B, ldb, info)
-            # chkmagmaerror(info[])
             B, A, ipiv
         end
     
         function magma_gesv!(A::Array{$elty}, B::Array{$elty})
-            # require_one_based_indexing(A, B)
-            # chkstride1(A, B)
+            @assert !has_offset_axes(A, B)
+            chkstride1(A, B)
             n = checksquare(A)
             isGPU = (A isa CuArray && B isa CuArray)
-            # if size(B,1) != n
-            #     throw(DimensionMismatch("B has leading dimension $(size(B,1)), but needs $n"))
-            # end
+            if size(B,1) != n
+                throw(DimensionMismatch("B has leading dimension $(size(B,1)), but needs $n"))
+            end
             lda  = max(1,stride(A,2))
             ldb  = max(1,stride(B,2))
             ipiv = similar(Matrix(A), Int32, n)
@@ -160,15 +136,6 @@ for type in magmaTypeList
                 A, lda, ipiv,
                 B, ldb, info
             )
-            # ccall((@magmafunc($gesv), libmagma), Cint,
-            #       (Cint, Cint,
-            #        PtrOrCuPtr{$elty}, Cint, Ptr{Cint},
-            #        PtrOrCuPtr{$elty}, Cint, Ptr{Cint}),
-            #        n, size(B,2),
-            #        A, lda, ipiv,
-            #        B, ldb, info)
-
-            # chkmagmaerror(info[])
             B, A, ipiv
         end
 
@@ -181,9 +148,8 @@ for type in magmaTypeList
         #      DOUBLE PRECISION   A( LDA, * ), B( LDB, * )
         function magma_getrs!(trans::magma_trans_t, A::CuMatrix{$elty}, ipiv::Array{Cint}, B::CuArray{$elty})
 
-            # require_one_based_indexing(A, ipiv, B)
-            # chktrans(trans)
-            # chkstride1(A, B, ipiv)
+            @assert !has_offset_axes(A, ipiv, B)            # chktrans(trans)
+            chkstride1(A, B, ipiv)
             @assert !has_offset_axes(A, ipiv, B)
             n = checksquare(A)
             if n != size(B, 1)
@@ -197,7 +163,6 @@ for type in magmaTypeList
             func = eval(@magmafunc_gpu($getrs))
             func(trans, n, nrhs, A, lda, ipiv, B, ldb, info)
             B
-            # println("\nThe info variable is: ", info, "\n")
         end
         function magma_getrs!(trans::magma_trans_t, A::Matrix{$elty}, ipiv::AbstractVector{Cint}, B::Array{$elty})
             magma_getrs!(trans, cu(A), ipiv, cu(B))
@@ -216,8 +181,7 @@ for type in magmaTypeList
         #*#         < 0: if INFO = -i, the i-th argument had an illegal value
         #*#         > 0: if INFO = i, U(i,i) is exactly zero; the matrix is singular and its cannot be computed.
         function magma_getri!(A::CuMatrix{$elty}, ipiv::Array{Int})
-            # require_one_based_indexing(A, ipiv)
-            # chkstride1(A, ipiv)
+            @assert !has_offset_axes(A, ipiv)            # chkstride1(A, ipiv)
             n = checksquare(A)
             if n != length(ipiv)
                 throw(DimensionMismatch("ipiv has length $(length(ipiv)), but needs $n"))
@@ -229,19 +193,8 @@ for type in magmaTypeList
             lwork = ceil(Int, real(n * nb))
             work  = cu(Vector{$elty}(undef, max(1, lwork)))
             info  = Ref{Cint}()
-            # for i = 1:2  # first call returns lwork as work[1]
-                # ccall((@magmafunc_gpu($getri), libmagma), Cvoid, # ! MAGMA has no native cpu interface for getri
-                #       (Ref{Cint}, PtrOrCuPtr{$elty}, Ref{Cint}, PtrOrCuPtr{Cint},
-                #        PtrOrCuPtr{$elty}, Ref{Cint}, PtrOrCuPtr{Cint}),
-                #       n, A, lda, ipiv, work, lwork, info)
-                # # chkmagmaerror(info[])
             func = eval(@magmafunc_gpu($getri))
             func(n, A, lda, ipiv, work, lwork, info)
-            #     if i == 1
-            #         lwork = ceil(Int, real(work[1]))
-            #         resize!(work, lwork)
-            #     end
-            # end
             A
         end
         function magma_getri!(A::Matrix{$elty}, ipiv::Array{Int})
