e7471091ff4d969d18ef86d2bb48cb4ba97e061f
Jerry <chenys13@outlook.com>
2019-08-29 18:59:12 -0400

    Edited

--- src/dense/factorization.jl
+++ src/dense/factorization.jl
@@ -11,7 +11,29 @@ for type in magmaTypeList
     
     @eval begin
 
-        function magma_geqrf!(A::)
+        function magma_geqrf!(A::Matrix{$elty})
+            require_one_based_indexing(A, tau)
+            chkstride1(A,tau)
+            m, n  = size(A)
+            if length(tau) != min(m,n)
+                throw(DimensionMismatch("tau has length $(length(tau)), but needs length $(min(m,n))"))
+            end
+            work  = Vector{$elty}(undef, 1)
+            lwork = BlasInt(-1)
+            info  = Ref{BlasInt}()
+            for i = 1:2                # first call returns lwork as work[1]
+                ccall((@blasfunc($geqrf), liblapack), Cvoid,
+                      (Ref{BlasInt}, Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt},
+                       Ptr{$elty}, Ptr{$elty}, Ref{BlasInt}, Ptr{BlasInt}),
+                      m, n, A, max(1,stride(A,2)), tau, work, lwork, info)
+                chklapackerror(info[])
+                if i == 1
+                    lwork = BlasInt(real(work[1]))
+                    resize!(work, lwork)
+                end
+            end
+            A, tau
+        end
 
     end
 
