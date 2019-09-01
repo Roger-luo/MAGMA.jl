371a9190a0d45ed045ba200535f2a366fd9479e7
Jerry <chenys13@outlook.com>
2019-09-01 16:34:04 -0400

    Fixed

--- test/dense/eigenvalues.jl
+++ test/dense/eigenvalues.jl
@@ -2,9 +2,9 @@
     # complex is easier for now
     @testset for elty in MAGMA.magmaTypeTuple
         A = rand(elty,10,10)
-        magma_result = MAGMA.magma_geev!('N','V',copy(A))
-        lapack_result= LAPACK.geev!('N', 'V', copy(A))
-        for i in 1:length(magma_result)
+        magma_result = MAGMA.magma_geev!('N','N',copy(A))
+        lapack_result= LAPACK.geev!('N', 'N', copy(A))
+        for i in 1:length(magma_result)-2
             println("Testing the $(i)th result: ")
             @test magma_result[i] ≈ lapack_result[i]
         end
