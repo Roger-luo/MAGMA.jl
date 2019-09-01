c87163e03901d79d3f17cea8b629e175edb75c8f
Jerry <chenys13@outlook.com>
2019-09-01 14:32:52 -0400

    Set up the functions' name to wrap

---
+++ src/dense/eigenvalues.jl
@@ -0,0 +1,4 @@
+const function_list_eigenvalues = ("geev", "gehrd", 
+                                   "heevx", "heevd", "heevdx", "heevr", "hetrd",
+                                   "hegvx", "hegvd", "hegvdx", "hegvr")
+
